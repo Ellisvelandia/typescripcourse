aaa8207d8ec54bef4597f6ae24cfe917d91eb18b
ellisvelandia <eyis619@gmail.com>
2022-12-06 13:19:52 -0500

    tuple

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
 const userName = "Ellis";
 const age = 29;
 
+console.log(`Hello world , my names is ${userName} i'm ${age} yeras old.`);
 
-console.log(`Hello world , my names is ${userName} i'm ${age} yeras old.`)
+// Basic types
+let myTypeString: string = "Hello world";
+let myTypeNumber: number = 29;
+let myTypeBoolean: boolean = true;
+
+let arrNumber: number[] = [1, 2, 3];
+// let arrNumber2 : Array<number> = [1,2,3];
+let arrString: string[] = ["Uno", "Dos"];
+
+let assAny: any[] = ["Hello", 2, true];
+
+// Tupple
+let tuplePlayers: [string, number, boolean] = ["Donic", 3, true];
+
+// Tuple Arrays
+
+let players: [number, string][];
+
+players = [
+  [1, "Lebron"],
+  [2, "jebron"],
+  [3, "uebron"],
+];
