2e87839ff7654f168a3c6ca92a1929bcee7abc05
ellisvelandia <eyis619@gmail.com>
2022-12-06 15:49:35 -0500

    any

--- index.ts
+++ index.ts
@@ -1,59 +1,70 @@
-// const userName = "Ellis";
-// const age = 29;
+// // // const userName = "Ellis";
+// // // const age = 29;
 
-// console.log(`Hello world , my names is ${userName} i'm ${age} yeras old.`);
+// // // console.log(`Hello world , my names is ${userName} i'm ${age} yeras old.`);
 
-// // Basic types
-// // let myTypeString: string = "Hello world";
-// // let myTypeNumber: number = 29;
-// // let myTypeBoolean: boolean = true;
+// // // // Basic types
+// // // // let myTypeString: string = "Hello world";
+// // // // let myTypeNumber: number = 29;
+// // // // let myTypeBoolean: boolean = true;
 
-// // let arrNumber: number[] = [1, 2, 3];
-// // let arrNumber2 : Array<number> = [1,2,3];
-// // let arrString: string[] = ["Uno", "Dos"];
+// // // // let arrNumber: number[] = [1, 2, 3];
+// // // // let arrNumber2 : Array<number> = [1,2,3];
+// // // // let arrString: string[] = ["Uno", "Dos"];
 
-// // let assAny: any[] = ["Hello", 2, true];
+// // // // let assAny: any[] = ["Hello", 2, true];
 
-// // Tupple
-// // let tuplePlayers: [string, number, boolean] = ["Donic", 3, true];
+// // // // Tupple
+// // // // let tuplePlayers: [string, number, boolean] = ["Donic", 3, true];
 
-// // Tuple Arrays
+// // // // Tuple Arrays
 
-// // let players: [number, string][];
+// // // // let players: [number, string][];
 
-// // players = [
-//   // [1, "Lebron"],
-//   // [2, "jebron"],
-//   // [3, "uebron"],
-// // ];
-
-
-
-// let user  = {name: "ellis", age: 29}
-
-// console.log("user.name")
-
-
-let greetings: string = "hello ellis"
-
-
-let mynum = 6
-
-greetings.toLowerCase()
-
-console.log(greetings)
-
-//number
-
-let userId: number = 334455.3
-// userId = 334455.5
-
-userId.toFixed()
-// userId = "ellis"
-//boolean
-
-let isLoggedIn: boolean = false
+// // // // players = [
+// // //   // [1, "Lebron"],
+// // //   // [2, "jebron"],
+// // //   // [3, "uebron"],
+// // // // ];
 
 
 
-export {}
+// // // let user  = {name: "ellis", age: 29}
+
+// // // console.log("user.name")
+
+
+// // let greetings: string = "hello ellis"
+
+
+// // let mynum = 6
+
+// // greetings.toLowerCase()
+
+// // console.log(greetings)
+
+// // //number
+
+// // let userId: number = 334455.3
+// // // userId = 334455.5
+
+// // userId.toFixed()
+// // // userId = "ellis"
+// // //boolean
+
+// // let isLoggedIn: boolean = false
+
+// // // 
+
+// // export {}
+
+
+// // ANY
+
+// let hero: string;
+
+// function getHero() {
+//   return "thor"
+// }
+
+// hero = getHero()
