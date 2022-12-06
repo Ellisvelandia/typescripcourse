b379f9699f74cda5dcc76f77227076fa74a6d72d
ellisvelandia <eyis619@gmail.com>
2022-12-06 16:11:59 -0500

    functions

---
+++ function.ts
@@ -0,0 +1,24 @@
+function addTwo(num: number) {
+ 
+  return num + 2 
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase()
+}
+
+
+function signUpUser(name:string, email: string, isPaid:boolean) {
+  
+}
+
+
+function logInUser(name:string, email: string, isPaid: boolean) {
+}
+
+
+addTwo(5)
+getUpper("ellis")
+signUpUser("crisanto","ellis@gmail.com",false)
+logInUser("x","e@hotmail.com",true)
+export {}
