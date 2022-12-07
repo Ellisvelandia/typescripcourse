36473ad7b080cd59e1eb4ada44accfc343e9610b
ellisvelandia <eyis619@gmail.com>
2022-12-07 04:54:20 -0500

    function any

--- function.ts
+++ function.ts
@@ -1,24 +1,43 @@
-function addTwo(num: number) {
- 
-  return num + 2 
-}
+// function addTwo(num: number) {
+//   return num + 2;
+// }
 
-function getUpper(val: string) {
-  return val.toUpperCase()
-}
+// function getUpper(val: string) {
+//   return val.toUpperCase();
+// }
 
+// function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-function signUpUser(name:string, email: string, isPaid:boolean) {
-  
-}
+// function logInUser(name: string, email: string, isPaid: boolean) {}
 
+// addTwo(5);
+// getUpper("ellis");
+// signUpUser("crisanto", "ellis@gmail.com", false);
+// logInUser("x", "e@hotmail.com", true);
+// export {};
 
-function logInUser(name:string, email: string, isPaid: boolean) {
-}
+// // function getValue(myVal:number): boolean {
+// //   if (myval > 5) {
+// //     return true
+// //   }
+// //   return "200 ok"
+// // }
 
+// const getHello = (s: string): string => {
+//   return "";
+// };
 
-addTwo(5)
-getUpper("ellis")
-signUpUser("crisanto","ellis@gmail.com",false)
-logInUser("x","e@hotmail.com",true)
-export {}
+// const heros = ["thor", "spiderman", "iroman"];
+// // const heros = [1, 2, 3]
+
+// heros.map((hero): string => {
+//   return `hero is ${hero}`;
+// });
+
+// function consoleError(errmsg: string): void {
+//   console.log(errmsg);
+// }
+
+// function handleError(errmsg: string): void {
+//   throw new Error(errmsg);
+// }
