fdf7064f33bb5d999cdbf35500d2d2c1ab744b0b
ellisvelandia <eyis619@gmail.com>
2022-12-07 05:30:52 -0500

    array

--- myobjects.ts
+++ myobjects.ts
@@ -1,67 +1,67 @@
-// // const User = {
-// //   name: "ellis",
-// //   email: "ellis@hotmail.com",
-// //   isAvtive: true
+// // // const User = {
+// // //   name: "ellis",
+// // //   email: "ellis@hotmail.com",
+// // //   isAvtive: true
+// // // }
+
+// // // // function createUser({name: string, isPaid: boolean}) {}
+
+// // // let newUser = {name: "ellis", isPaid: false, email: "ellis@hotmail.com"}
+
+// // // createUser(newUser)
+
+// // // function createCourse():{name:string, price: number} {
+// // //   return { name: "reactjs", price: 399}
+// // // }
+
+
+
+// // type User = {
+// //   name: string;
+// //   email: string;
+// //   isActive: boolean
 // // }
 
-// // // function createUser({name: string, isPaid: boolean}) {}
 
-// // let newUser = {name: "ellis", isPaid: false, email: "ellis@hotmail.com"}
-
-// // createUser(newUser)
-
-// // function createCourse():{name:string, price: number} {
-// //   return { name: "reactjs", price: 399}
+// // function createUser(user:User):   User {
+// //   return {name: "", email: "", isActive: true}
 // // }
 
+// // createUser({name: "", email: "", isActive: true})
 
 
-// type User = {
-//   name: string;
-//   email: string;
+// type User =  {
+//   readonly _id: string
+//   name: string
+//   email: string
 //   isActive: boolean
+//   creadcardDetails?: number
+// }
+
+// // function createUser(u:User) {}
+// let myUser: User = {
+//   _id: "12345",
+//   name: "h",
+//   email: "h@h.com",
+//   isActive: false
+// }
+
+// myUser.email = "ellis@hotmail.com"
+// // myUser._id = "asa"
+
+// type cardNumber = {
+//   cardNumber: String
+// }
+
+// type cardDate = {
+//   cardDate: string
+// }
+
+// type cardDetails = cardNumber & cardDate & {
+//   cvv: number
 // }
 
 
-// function createUser(user:User):   User {
-//   return {name: "", email: "", isActive: true}
-// }
-
-// createUser({name: "", email: "", isActive: true})
 
 
-type User =  {
-  readonly _id: string
-  name: string
-  email: string
-  isActive: boolean
-  creadcardDetails?: number
-}
-
-// function createUser(u:User) {}
-let myUser: User = {
-  _id: "12345",
-  name: "h",
-  email: "h@h.com",
-  isActive: false
-}
-
-myUser.email = "ellis@hotmail.com"
-// myUser._id = "asa"
-
-type cardNumber = {
-  cardNumber: String
-}
-
-type cardDate = {
-  cardDate: string
-}
-
-type cardDetails = cardNumber & cardDate & {
-  cvv: number
-}
-
-
-
-
-export{}
+// export{}
