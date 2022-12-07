0239533dd2746ecfd5de1dd13c04325426498da6
ellisvelandia <eyis619@gmail.com>
2022-12-07 05:52:25 -0500

    unions

---
+++ myUnion.ts
@@ -0,0 +1,42 @@
+// let score: number | string = 33;
+// score = 44;
+// score = "55";
+
+// type User = {
+//   name: string;
+//   id: number
+// }
+
+// type Admin = {
+//   username: string;
+//   id: number
+// }
+
+// let ellis: User | Admin = {name: "ellis", id: 344}
+
+// ellis = {username: "hc", id: 334}
+
+// // function getDbId(id:number | string) {
+// //   console.log(`DB id is: ${id}`)
+// // }
+
+
+// getDbId(3)
+// getDbId("3")
+
+// function getDbId(id:number | string) {
+//   if (typeof id === "string") {
+//     id.toLocaleLowerCase()
+//   }
+// }
+
+
+// //array
+// const data: number[] = [1,2,3]
+// const data2: string[] = ["1","2","3"]
+// const data3: (string | number | boolean)[] = ["1", "2", 3, true]
+
+// let seatAllowtment: "aisle"  | "midle" | "window"
+
+// // seatAllowtment = "asile"
+// // seatAllowtment = "crew"
